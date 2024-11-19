d2fab38fb0c9371404564e79031bef10ef24373b
Yakubu Esuza Odiara <yakubuesuza@gmail.com>
2024-11-19 22:09:48 +0300

    Finish lab

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
+// Import user data
 import user from "../data/user";
 console.log(user);
 
@@ -11,8 +11,10 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      {/* Pass the necessary props to Home */}
+      <Home name={user.name} city={user.city} color={user.color} />
+      {/* Pass the bio and links to About */}
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
